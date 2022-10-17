a02e68abc5383484b273f602077da7f56c7fae63
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-10-17 15:58:08 +0900

    10/17

--- src/sections/contact/ConfirmationModal.js
+++ src/sections/contact/ConfirmationModal.js
@@ -3,20 +3,18 @@ import styles from "./ConfirmationModal.module.css";
 export default function ConfirmationModal({ sendFailed, closeHandler }) {
   return (
     <div className={`flex-col ${styles["confirmation-modal"]}`}>
-      {!sendFailed ? (
-        <>
-          <h1 className="page-header">Thank you for reaching out!</h1>
-          <p className="text">
-            Your email has been sent. Please allow 1-2 business days for a
-            reply.
-          </p>
-        </>
-      ) : (
-        <>
-          <h1>Oh no! An error occured.</h1>
-          <p className={styles.text}>{sendFailed}</p>
-        </>
-      )}
+      <>
+        <h1 className="page-header">
+          {!sendFailed
+            ? "Thank you for reaching out!"
+            : "Oh no! An error occured."}
+        </h1>
+        <p className="text">
+          {!sendFailed
+            ? "Your email has been sent. Please allow 1-2 business days for a reply."
+            : { sendFailed }}
+        </p>
+      </>
       <button className={`text ${styles.btn}`} onClick={closeHandler}>
         Got it
       </button>
