e9694db032dd15d5cbd1c0fc164bf42dd45117bc
Knikkey <102648803+Knikkey@users.noreply.github.com>
2022-12-01 08:55:16 +0900

    contactMe email fail test

--- cypress/e2e/contactMe.cy.js
+++ cypress/e2e/contactMe.cy.js
@@ -1,5 +1,5 @@
 describe("Contact Me", () => {
-  it("successful send", () => {
+  it("failed send modal appears properly", () => {
     //init
     cy.visit("https://nick-bingham.netlify.app/");
     cy.get(":nth-child(4) > .NavItems_page-nav__-0Gj-").click();
@@ -18,8 +18,7 @@ describe("Contact Me", () => {
 
     //send
     cy.get(".Contact_btn__3sduE").click();
-    cy.wait(3000);
 
-    cy.contains("Thank you for reaching out!").should("be.visible");
+    cy.contains("error").should("be.visible");
   });
 });
