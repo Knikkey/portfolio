17ec701f055cb5ce2623ffc0704f6207e4e25f4b
Knikkey <102648803+Knikkey@users.noreply.github.com>
2023-02-07 10:21:24 +0900

    changed resume

--- src/components/navbar/NavItems.js
+++ src/components/navbar/NavItems.js
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./NavItems.module.css";
-import resume from "../resume/Nicholas Bingham Resume.pdf";
+import resume from "../resume/Nicholas Bingham Resume - public.pdf";
 import menuIcon from "./menuIcon.svg";
 
 const pages = [
